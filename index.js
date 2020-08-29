91b8befc50c1eade9c2e23ac801ea92523284812
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-08-29 12:20:08 +0200

    add a ping command

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('message', message => {
+    console.log(message.content);
+    if (message.content === ',ping') {
+        message.channel.send('pong');
+    }
+});
+
+
 client.login(process.env.TOKEN);
+
