c88a98513a2defc60dadfc1cca7143bdfdfee5bb
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-08-29 13:27:40 +0200

    Add more commands

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const { prefix } = require('./config.json');
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
+
 dotenv.config();
 const client = new Discord.Client();
 
@@ -7,13 +22,26 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+function makeDate(author){
+    return`${author.createdAt.getDate()}.${author.createdAt.getMonth()}.${author.createdAt.getFullYear()}` //nice code
+}
+
 client.on('message', message => {
-    console.log(message.content);
-    if (message.content === ',ping') {
+    if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('pong');
+    }else if (message.content.startsWith(`${prefix}user`)) {
+        const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#16a085')
+            .setTitle('User info')
+            .setThumbnail(message.author.displayAvatarURL())
+            .addFields(
+                {name: "Username : ", value: message.author.username},
+                { name: 'Your ID : ', value: message.author.id },
+                { name: 'Account created on : ', value: makeDate(message.author), inline: true },
+                { name: 'Inline field title', value: message.author.createdTimestamp, inline: true },
+            )
+        message.channel.send(exampleEmbed);
     }
 });
 
-
 client.login(process.env.TOKEN);
-
