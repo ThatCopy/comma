75ee1a8255a9740157766108137b4786ddcbce79
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-05 13:26:09 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'cat',
 	description: 'Get a cat photo',
 	async execute(message, args, client) {
-        rq.get("https://thatcopy.pw/catapi/rest", (error, response, body) => {
+        rq.get("https://www.thatcopy.pw/catapi/rest/", (error, response, body) => {
             let json = JSON.parse(body)
             let embed = new Discord.MessageEmbed()
                 .setImage(json.webpurl)
