e2e3cbb0c390f7cd462d2f3ba8487491f48a1462
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-19 17:06:52 +0200

    Fix the config and switch to node-fetch

--- commands/fact.js
+++ commands/fact.js
@@ -1,23 +1,17 @@
-const request = require("request")
+const fetch = require('node-fetch')
 const Discord = require("discord.js")
 
 module.exports = {
 	name: 'fact',
 	description: 'Tells you a random fact',
 	async execute(message, args, client) {
-        request.get("https://uselessfacts.jsph.pl/random.json?language=en", (error, response, body) => {
-            if(error){
-                message.channel.send("Something went wrong")
-                return
-            }
-            let parsedText = JSON.parse(body)
-            let text = parsedText.text
-            let safeText = text.replace(/`/g, "'")
-
-            let embed = new Discord.MessageEmbed()
-                .setColor("#3bffba")
-                .setDescription(safeText)
-            message.channel.send(embed)
-        })
-    }
+		let res = await fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+		let json = await res.json()
+		let text = json.text
+		let safeText = text.replace(/`/g, "'")
+		let embed = new Discord.MessageEmbed()
+			.setColor("#3bffba")
+			.setDescription(safeText)
+		message.channel.send(embed)
+	}
 }
