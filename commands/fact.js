683d7b31092ceda54017a2c0f85a7510785ee003
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-19 11:55:50 +0200

    Add the fact command

--- commands/fact.js
+++ commands/fact.js
@@ -2,8 +2,8 @@ const request = require("request")
 const Discord = require("discord.js")
 
 module.exports = {
-	name: 'game',
-	description: 'rock paper scissors',
+	name: 'fact',
+	description: 'Tells you a random fact',
 	async execute(message, args, client) {
         request.get("https://uselessfacts.jsph.pl/random.json?language=en", (error, response, body) => {
             if(error){
@@ -14,8 +14,8 @@ module.exports = {
             let text = parsedText.text
             let safeText = text.replace(/`/g, "'")
 
-            let embed = Discord.MessageEmbed()
-                .setColor("#ffffff")
+            let embed = new Discord.MessageEmbed()
+                .setColor("FFFFFF")
                 .setDescription(safeText)
             message.channel.send(embed)
         })
