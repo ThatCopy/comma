2b06b4ae4c3b709d3fe797a69df4b714e8f13958
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-11-02 10:11:26 +0100

    Initial command stuff and a poc

---
+++ commands/parrot.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "parrot",
+    description: "Gives you a random party parrot",
+    execute(message, args, client) {
+        message.channel.send("<a:exceptionallyfastparrot:772747640051728404>");
+    }
+}
