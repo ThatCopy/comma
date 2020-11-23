2661f99e127651c71f43733bc47953260b3b6b5f
copy <copy@DESKTOP-O5IFB3O.localdomain>
2020-11-23 13:13:27 +0100

    Refactor the joke command

--- commands/joke.js
+++ commands/joke.js
@@ -3,62 +3,41 @@ const fetch = require('node-fetch')
 
 let re = /\b(pun|programming|miscellaneous|dark)\b/i
 
+async function getJoke(urlParams = "Any") {
+    let res = await fetch("https://sv443.net/jokeapi/v2/joke/" + urlParams + "?blacklistFlags=nsfw")
+    if(res.status != 200) {throw new Error("📡 Couldn't connect to JokeAPI.")}
+    let json = await res.json()
+    if(json.type == "single"){return json.joke}
+    else {return `**${json.setup}** \n\n ${json.delivery}`}
+}
+
+function invalidCats(message) {
+    let embed = new Discord.MessageEmbed()
+        .setColor("#8a2de1")
+        .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
+        .setTitle("Error, joke category is not valid. Categories:")
+        .setDescription("- Miscellaneous\n- Programming\n- Dark\n- Pun\n\nFor Any leave the flags empty.")
+    message.channel.send(embed);
+}
+
 module.exports = {
 	name: 'joke',
 	description: 'tells you a joke',
 	async execute(message, args, client) {
-        try{
-        if(args.length){
-            if(re.test(args[0])){
-                let res = await fetch("https://sv443.net/jokeapi/v2/joke/" + args[0] + "?blacklistFlags=nsfw")
-                let json = await res.json()
-                if(json.type == "single"){
-                    var embed = new Discord.MessageEmbed()
-                        .setTitle(json.joke)
-                        .setColor("#8a2de1")
-                        .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
-                    message.channel.send(embed);
-                }
-                else if(json.type == "twopart"){
-                    var embed = new Discord.MessageEmbed()
-                        .setDescription(`**${json.setup}** \n\n ${json.delivery}`)
-                        .setColor("#8a2de1")
-                        .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
-                    message.channel.send(embed)
-                    }
-                }
-            else{
-                var embed = new Discord.MessageEmbed()
-                    .setColor("#8a2de1")
-                    .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
-                    .setTitle("Error, joke category is not valid. Categories:")
-                    .setDescription("- Miscellaneous\n- Programming\n- Dark\n- Pun\n\nFor Any leave the flags empty.")
-                message.reply(embed);
-            }
+        var joke;
+        if(args.length && !re.test(args[0])) { return invalidCats(message) }
+        try {
+            if(args.length){ joke = await getJoke(args[0])}            
+            else { joke = await getJoke() }
+        } catch (error) {
+            
         }
-        else{
-            let res = await fetch("https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw")
-            let json = await res.json()
-            if(json.type == "single"){
-                var embed = new Discord.MessageEmbed()
-                    .setTitle(json.joke)
-                    .setColor("#8a2de1")
-                    .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
-                message.channel.send(embed);
-                }
-                else if(json.type == "twopart"){
-                    var embed = new Discord.MessageEmbed()
-                        .setDescription(`**${json.setup}** \n\n ${json.delivery}`)
-                        .setColor("#8a2de1")
-                        .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
-                    message.channel.send(embed)
-                }
-                else{
-                    message.reply("📡 Couldn't connect to JokeAPI.");
-                }
-            }
-        } catch(e) {
-            message.reply("📡 Couldn't connect to JokeAPI.");
-        }
+
+        let embed = new Discord.MessageEmbed()
+            .setTitle(joke)
+            .setColor("#8a2de1")
+            .setFooter("Powered by JokeAPI - https://jokeapi.dev ")
+        message.channel.send(embed);
+
     }
-    }
+}
