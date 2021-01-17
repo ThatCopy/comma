7183a0cffffeec5391efaba56242082e535a407e
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2021-01-17 20:30:08 +0100

    make sure no nsfw stuff goes tru

--- commands/meme.js
+++ commands/meme.js
@@ -5,13 +5,19 @@ module.exports = {
 	name: 'meme',
     description: 'get a meme',
     async execute(message, args, client) {
-        let res = await fetch("https://meme-api.herokuapp.com/gimme")
-            let json = await res.json()
-            var embed = new Discord.MessageEmbed()
-                .setColor("#FF5700")
-                .setTitle(json.title)
-                .setImage(json.url)
-                .setFooter(`Meme made by ${json.author}. Powered by Meme_Api - https://github.com/R3l3ntl3ss/Meme_Api`)
-            message.channel.send(embed);
+        let safe = false
+        let res, json
+        while(!safe){
+            res = await fetch("https://meme-api.herokuapp.com/gimme")
+            json = await res.json() 
+            if(json.nsfw || !json.spoiler) { return }
+            safe = true
         }
+        var embed = new Discord.MessageEmbed()
+            .setColor("#FF5700")
+            .setTitle(json.title)
+            .setImage(json.url)
+            .setFooter(`Meme made by ${json.author}. Powered by Meme_Api - https://github.com/R3l3ntl3ss/Meme_Api`)
+        message.channel.send(embed);
     }
+}
