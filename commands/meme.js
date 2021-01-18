6029c1fe47e5b4010ec068f7bcced2f3b8cd5bfd
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2021-01-18 19:44:40 +0100

    fix the meme command

--- commands/meme.js
+++ commands/meme.js
@@ -5,19 +5,18 @@ module.exports = {
 	name: 'meme',
     description: 'get a meme',
     async execute(message, args, client) {
-        let safe = false
+        let explicit = true
         let res, json
-        while(!safe){
+        while(explicit){
             res = await fetch("https://meme-api.herokuapp.com/gimme")
             json = await res.json() 
-            if(json.nsfw == true || json.spoiler == false) { return }
-            safe = true
+            if(!json.nsfw || !json.spoiler) { explicit = false }
         }
         var embed = new Discord.MessageEmbed()
             .setColor("#FF5700")
             .setTitle(json.title)
             .setImage(json.url)
             .setFooter(`Meme made by ${json.author}. Powered by Meme_Api - https://github.com/R3l3ntl3ss/Meme_Api`)
-        message.channel.send(embed);
+        message.channel.send(embed)
     }
 }
